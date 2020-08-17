4519c10e7293cbd1046454afa6bf214a6b1485d1
Rudolph Pereira <rudolph.pereira93@gmail.com>
2020-08-17 09:27:10 +0530

    Added filter option

--- app.js
+++ app.js
@@ -2,10 +2,12 @@
 const todoInput = document.querySelector(".todo-input");
 const todoBtn = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".filter-todo");
 
 // Event Listeners
 todoBtn.addEventListener("click", addTodo);
 todoList.addEventListener("click", deleteTodo);
+filterOption.addEventListener("click", filterTodo);
 
 // Functions
 function addTodo(e) {
@@ -35,9 +37,7 @@ function addTodo(e) {
 
 function deleteTodo(e) {
   e.preventDefault();
-
   const item = e.target;
-
   // Delete Button
   if (item.classList[0] === "trash-btn") {
     const todo = item.parentElement;
@@ -53,4 +53,29 @@ function deleteTodo(e) {
     const todo = item.parentElement;
     todo.classList.toggle("completed");
   }
+}
+
+function filterTodo(e) {
+  const todos = todoList.childNodes;
+  todos.forEach(function (todo) {
+    switch (e.target.value) {
+      case "all":
+        todo.style.display = "flex";
+        break;
+      case "completed":
+        if (todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+      case "uncompleted":
+        if (!todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+    }
+  });
 }
